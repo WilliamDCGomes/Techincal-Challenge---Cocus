18444e239dc716eccf7105a0bb2e5774945f2062
William Douglas <williamdouglasgomes@hotmail.com>
2026-06-24 13:46:49 +0100

    Blocked layout rotation in devices

--- src/WeatherApp/Platforms/Android/MainActivity.cs
+++ src/WeatherApp/Platforms/Android/MainActivity.cs
@@ -6,7 +6,7 @@ using AndroidX.Core.View;
 
 namespace WeatherApp;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
     protected override void OnCreate(Bundle? savedInstanceState)
